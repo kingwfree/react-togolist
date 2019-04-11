e1529d187d5cbb822b45a58769ba78244667ab06
kingwfree <1320054439@qq.com>
2019-04-11 20:18:27 +0800

    已完成

---
+++ src/static/js/data.js
@@ -0,0 +1,66 @@
+const dataList = {
+    "colleagues": {
+        "name": "同事",
+        "list": [
+            {
+                id: Math.random(),
+                "name": "阿飞飞",
+                "gender": "男",
+                "introduce": "已结婚生子的男人"
+            },
+            {
+                id: Math.random(),
+                "name": "心艾宝宝",
+                "gender": "男 and 女",
+                "introduce": "你们的心艾麻麻很可爱w"
+            },
+            {
+                id: Math.random(),
+                "name": "乌拉拉",
+                "gender": "女",
+                "introduce": "大家心目中的代码女神"
+            }
+        ]
+    },
+    "netFriend": {
+        "name": "网友",
+        "list": [
+            {
+                id: Math.random(),
+                "name": "周星弛",
+                "gender": "男",
+                "introduce": "经典电影"
+            },
+            {
+                id: Math.random(),
+                "name": "李连杰",
+                "gender": "男",
+                "introduce": "功夫皇帝"
+            }
+        ]
+    },
+    "students": {
+        "name": "基友",
+        "list": [
+            {
+                id: Math.random(),
+                "name": "狗蛋",
+                "gender": "男",
+                "introduce": "能吃"
+            },
+            {
+                id: Math.random(),
+                "name": "老王",
+                "gender": "男",
+                "introduce": "能睡"
+            },
+            {
+                id: Math.random(),
+                "name": "大锤",
+                "gender": "男",
+                "introduce": "能逗"
+            }
+        ]
+    }
+}
+
